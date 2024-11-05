c9a8b22a2a470853c51c26ed7721496c412bfb6d
vidyasagar2110 <tvidyasagar2110@gmail.com>
2024-11-05 14:22:59 +0530

    Delete fashion_matcher directory

--- fashion_matcher/matcher/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('male/', views.male_preferences, name='male_preferences'),
-    path('female/', views.female_preferences, name='female_preferences'),
-]
